452c56cda614f6ce34900fe7c8226f33ffc7ecf5
Gabriel <gabriel.f.silva117@gmail.com>
2023-02-28 10:44:26 -0300

    feat: Adds the Tech Component

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -3,6 +3,8 @@ import AboutMe from './aboutMe/AboutMe'
 import './header/header.css'
 import HeaderBar from './header/HeaderBar'
 import InfoContainer from './info/InfoContainer'
+import TechContainer from './tech/TechContainer'
+
 const HomePage = () => {
   return (
     <div>
@@ -14,6 +16,9 @@ const HomePage = () => {
       </section>
       <section className="aboutme-section">
         <AboutMe />
+      </section>
+      <section className="tech-section">
+        <TechContainer />
       </section>
     </div>
   )
