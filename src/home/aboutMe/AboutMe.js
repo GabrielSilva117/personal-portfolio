ba8a54db9a81767df94223bdaac711d6fc595790
Gabriel <gabriel.f.silva117@gmail.com>
2023-05-21 19:55:58 -0300

    feat: Changes the 'about me' text

--- src/home/aboutMe/AboutMe.js
+++ src/home/aboutMe/AboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = () => {
         <h4>Sobre Mim</h4>
       </div>
       <div className="aboutme-text">
-        <p>
+        {/* <p>
           Sou Gabriel, tenho 20 anos e sempre fui fascinado por tecnologia e
           pela área de TI, atualmente estou cursando Engenharia de Software no
           <strong> Quarto semestre</strong> e estudo programação há mais de um
@@ -20,6 +20,16 @@ const AboutMe = () => {
           desenvolvi diversos projetos pessoais e um projeto FrontEnd como
           freelancer para uma empresa de eventos, onde, durante essa jornada,
           aprendi e utilizei as tecnologias abaixo.
+        </p> */}
+        <p>
+          Sou Gabriel, tenho 20 anos e sempre fui fascinado por tecnologia e
+          pela área de TI, atualmente estou cursando Engenharia de Software no
+          Quarto semestre, já trabalhei em um projeto Front End como freelancer
+          para uma empresa de eventos, e no momento atuo na área como
+          Desenvolvedor Full Stack para a WEG, onde auxilio no desenvolvimento e
+          na manutenção das aplicações da mesma. Possuo conhecimento tanto em
+          programação funcional quando em POO, e já desenvolvi/trabalhei com
+          aplicações envolvendo as tecnologia citadas na tabela abaixo.
         </p>
       </div>
     </div>
