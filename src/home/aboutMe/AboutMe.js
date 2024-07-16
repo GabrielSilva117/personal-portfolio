5c1ea2177ec604e74f92ed9c6505f0d321e333ed
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-07-16 19:23:44 -0300

    feat: Adds an age calculation logic to the aboutme text

--- src/home/aboutMe/AboutMe.js
+++ src/home/aboutMe/AboutMe.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import './aboutme.css'
 
+const getAge = (tDate) => {
+  const birthDate = new Date(tDate);
+  const curDate = new Date();
+
+  let age = curDate.getFullYear() - birthDate.getFullYear();
+
+  if (curDate.getMonth() < birthDate.getMonth()) {
+    age--;
+  }
+
+  return age;
+}
+
 const AboutMe = () => {
+  const myAge = getAge('2002-09-09');
+
   return (
     <div
       className="aboutme-container"
@@ -12,29 +27,10 @@ const AboutMe = () => {
         <h4>Sobre Mim</h4>
       </div>
       <div className="aboutme-text">
-        {/* <p>
-          Sou Gabriel, tenho 20 anos e sempre fui fascinado por tecnologia e
-          pela área de TI, atualmente estou cursando Engenharia de Software no
-          <strong> Quarto semestre</strong> e estudo programação há mais de um
-          ano, nunca trabalhei e não possuo experiência na área, entretanto
-          desenvolvi diversos projetos pessoais e um projeto FrontEnd como
-          freelancer para uma empresa de eventos, onde, durante essa jornada,
-          aprendi e utilizei as tecnologias abaixo.
-        </p> */}
-        {/* <p>
-          Sou Gabriel, tenho 20 anos e sempre fui fascinado por tecnologia e
-          pela área de TI, atualmente estou cursando Engenharia de Software no
-          Quarto semestre, já trabalhei em um projeto Front End como freelancer
-          para uma empresa de eventos, e no momento atuo na área como
-          Desenvolvedor Full Stack para a WEG, onde auxilio no desenvolvimento e
-          na manutenção das aplicações da mesma. Possuo conhecimento tanto em
-          programação funcional quando em POO, e já desenvolvi/trabalhei com
-          aplicações envolvendo as tecnologia citadas na tabela abaixo.
-        </p> */}
         <p>
-          Sou Gabriel tenho 20 anos e sempre fui fascinado por tecnologia e pela
-          área de TI, atualmente estou cursando Engenharia de Software no Quinto
-          semestre e atuo como desenvolvedor Full Stack Júnior na GEOvendas,
+          Sou Gabriel tenho {myAge} anos e sempre fui fascinado por tecnologia e pela
+          área de TI, atualmente estou cursando Engenharia de Software no Setimo
+          semestre e atuo como desenvolvedor Full Stack na GEOvendas,
           onde colaboro no desenvolvimento e na manutenção das aplicações da
           mesma. Possuo conhecimento tanto em programação funcional quanto em
           POO e já desenvolvi/trabalhei com aplicações envolvendo as tecnologia
